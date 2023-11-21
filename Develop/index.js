6666bfc0034bb5421a980cdb4b11b64c78cd5dda
Alexis Lopez <lopez111alex@gmail.com>
2023-11-20 18:02:12 -0800

    Added logic to index.js
    I required inquirer fs and markdown from the utils folder. Made a question array and created a function to initialize app

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const generateMarkdown = require("./generateMarkdown");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("README.md successfully generated!")
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdownContent = generateMarkdown(answers);
+    writeToFile("README.md", markdownContent);
+  });
+}
 
 // Function call to initialize app
 init();
