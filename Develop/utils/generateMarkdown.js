59513767d33b60e5bfa982a68ba5ee64e3ee189e
Alexis Lopez <lopez111alex@gmail.com>
2023-11-20 18:25:43 -0800

    Added logic to renderLicenseLink

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -19,7 +19,22 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "[MIT License](https://opensource.org/licenses/MIT)";
+    case "Apache-2.0":
+      return "[Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)";
+    case "GPL-3.0":
+      return "[GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0)";
+    case "BSD-2-Clause":
+      return "[BSD 2-Clause License](https://opensource.org/licenses/BSD-2-Clause)";
+    case "BSD-3-Clause":
+      return "[BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause)";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
