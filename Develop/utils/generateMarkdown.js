eace286079390c76f2b2171733e733cddd19cea7
Alexis Lopez <lopez111alex@gmail.com>
2023-11-20 18:24:55 -0800

    Added renderLicenseBadge logic
    Added 5 licence badges with a switch and caser statement

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)";
+    case "Apache-2.0":
+      return "![Apache License 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+    case "GPL-3.0":
+      return "![GNU General Public License v3.0](https://img.shields.io/badge/License-GNU%20GPLv3-blue.svg)";
+    case "BSD-2-Clause":
+      return "![BSD 2-Clause License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)";
+    case "BSD-3-Clause":
+      return "![BSD 3-Clause License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
