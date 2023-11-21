79a57d45975b0af054f335e82c75d032574f59f4
Alexis Lopez <lopez111alex@gmail.com>
2023-11-20 18:10:42 -0800

    Added more questions
    Added more questions for the user

--- Develop/index.js
+++ Develop/index.js
@@ -9,6 +9,34 @@ const questions = [
     name: "title",
     message: "What is the title of your project?",
   },
+  {
+    type: "input",
+    name: "Description",
+    message:
+      "Provide a short description explaining the what, why, and how of your project.",
+  },
+  {
+    type: "input",
+    name: "Installation",
+    message:
+      "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message:
+      "Provide instructions and examples for use. Include screenshots as needed.",
+  },
+  {
+    type: "choice",
+    name: "License",
+    message: "placeholder",
+  },
+  {
+    type: "input",
+    name: "Features",
+    message: "If your project has a lot of features, list them here.",
+  },
 ];
 
 // TODO: Create a function to write README file
